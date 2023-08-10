d0752080ed4082c9d77e1346f668751ffa646dd2
Heran Wang <hrwang17@gmail.com>
2023-08-10 13:44:15 -0700

    Add GlobalExceptionHandler and ErrorDetails to implement exception handling

---
+++ src/main/java/com/beshton/shopping/exception/ErrorDetails.java
@@ -0,0 +1,39 @@
+package com.beshton.shopping.exception;
+
+import org.springframework.cglib.core.Local;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+public class ErrorDetails {
+    private final LocalDateTime timestamp;
+    private final String message;
+    private final String details;
+
+    public ErrorDetails(LocalDateTime timestamp, String message, String details) {
+        this.timestamp = timestamp;
+        this.message = message;
+        this.details = details;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    @Override
+    public String toString() {
+        return "ErrorDetails{" +
+                "timestamp=" + timestamp +
+                ", message='" + message + '\'' +
+                ", details='" + details + '\'' +
+                '}';
+    }
+}
